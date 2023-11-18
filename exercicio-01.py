90aa8061facade25a39f37be67b66c2a2cf5baa6
eric5654 <ericsantos2526@gmail.com>
2023-11-17 21:39:23 -0300

    commet-exercicio-01

---
+++ exercicio-01.py
@@ -0,0 +1,41 @@
+#nome = "Eric"
+#idade = 19
+#altura = 1.90
+#estudante = True
+#
+#print(type(nome))
+#print(type(idade))
+#print(type(altura))
+#print(type(estudante))
+
+# operadores matematicos
+soma = 10 + 5 
+diferenca = 8 - 7
+produto = 8 * 6
+divisao = 10 / 5
+print(soma, diferenca,produto,divisao) 
+print("operadores matematicos")
+print("Soma: ", soma, "diferenca", diferenca, "Produto: ",produto ,"Divisao: " , divisao)
+ 
+# operadores de comparaçao
+
+igual = 5 == 7
+diferente = 5 != 8
+maior = 7 > 5
+menor = 8 < 6
+print("operadoresn de comparaçao")
+print("Igual: ", igual, "Diferente", diferente, "Maior: ",maior ,"Menor: " , menor)
+
+
+ 
+  #  operadores logicos
+
+e = True and False
+ou = True or False
+nao = not True
+
+print(e,ou,nao)
+
+print("operadores logico")
+print("e: ", e, "ou:" ou,"nao:", nao)
+ 
