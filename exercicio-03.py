87ef3e2a28ba150951de85e1a1ffa3a7da11333f
eric5654 <ericsantos2526@gmail.com>
2023-11-23 20:39:24 -0300

    commit exercicio-03.py

---
+++ exercicio-03.py
@@ -0,0 +1,9 @@
+idade = input("Digite a idade de Usuario")
+
+if idade >= 18:
+    print("Usuario é adulto")
+elif idade < - 18:
+    print("Usuario é um adolesçente")
+
+if idade  > + 60:
+    print ("Usuario é um idoso")
