b917f2c81480ad887f89395db0f525a29e10df3c
eric5654 <ericsantos2526@gmail.com>
2023-11-22 21:23:11 -0300

    commit exercicio-02

---
+++ exercicio-02.py
@@ -0,0 +1,13 @@
+idade = 20
+verdadeiro = True
+falso = False
+print (idade)
+if idade >= 18:
+    print("Usuario é maior de idade", idade)
+    else:
+        print("USUARIO MENOR DE IDADE",idade)
+status = True
+if status:
+    print("Verdadeiro")
+else:
+        print("Falso")
