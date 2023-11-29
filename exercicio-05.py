0224ec187fad432f38a6d848935a7e92282b8577
eric5654 <151091723+eric5654@users.noreply.github.com>
2023-11-29 19:37:44 -0300

    Update exercicio-05.py

--- exercicio-05.py
+++ exercicio-05.py
@@ -1,1 +1,13 @@
+suarios = {
+         'Icaro': '3456',
+         'Isabela': '4657',
+         'Jonatas':  '5676'
+}
 
+usuario = input("Informe seu Usuario: ")
+senha = input("Unforme a sua Senha")
+
+if usuario in Usuarios and Usuarios[usuario] == senha:
+   print("Login bem sucedido! ")
+else:
+      print("Usuario não existe!")
