5022bf35390a1faa26e8d710be6302d6e4f81862
eric5654 <ericsantos2526@gmail.com>
2023-11-28 21:15:55 -0300

    commit-exercicio-04

---
+++ exercicio-05.py
@@ -0,0 +1,14 @@
+Usuarios = ("Jonatas","Isabela","Icaro")
+Senhas = ("6547","7890","3456")
+
+Nome =  input("Informe o usuario:")
+Senhas = input("Informe a senha de usuario:")
+
+
+if Nome in Usuarios:
+
+elif Usuarios[Nome]==Senhas:
+    
+   print ("Login bem sucedido")
+else:
+    print ("Nome de usuario e senhas invalidos")
